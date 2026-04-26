9b7976301b173bd6ae8a5404f22be788e1965f84
Abhishek kumar <124759861+theabhinerd@users.noreply.github.com>
2026-04-27 01:45:16 +0530

    Delete SmartPay/src directory

--- SmartPay/src/exception/InvalidReadingException.java
+++
@@ -1,9 +0,0 @@
-package exception; 
- 
-public class InvalidReadingException extends Exception {
-
-    // Constructor to pass error message
-    public InvalidReadingException(String msg) {
-        super(msg);  
-    }
-}
--- SmartPay/src/model/Billable.java
+++
@@ -1,5 +0,0 @@
- package model;   // ✅ MUST be same as folder
-
-public interface Billable {
-    double calculateTotal();
-}
